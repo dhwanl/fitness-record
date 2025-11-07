abebcc7456932646c77d30c1df0d92981153a03e
Donghwan Lee <dhwanyi@gmail.com>
2025-11-07 08:54:13 -0800

    Update JsonReader for new model

--- src/test/persistence/JsonReaderTest.java
+++ src/test/persistence/JsonReaderTest.java
@@ -4,12 +4,9 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import java.io.IOException;
 import java.util.List;
-import java.util.ArrayList;
-
 import org.junit.jupiter.api.Test;
-
-import model.Log;
 import model.Muscles;
+import model.WorkoutSession;
 
 
 // Referenced from JsonSerialization Demo
@@ -18,24 +15,24 @@ public class JsonReaderTest extends JsonTest {
 
     @Test
     public void testReaderNonExistentFile() {
-        JsonReader reader = new JsonReader("./data/donghwan.json");
-        List<Log> logs = new ArrayList<Log>();
+        JsonReader reader = new JsonReader("./data/noSuchFile.json");
 
         try {
-            logs = reader.read();
+            reader.read();
             fail("IOExcetion expected");
         } catch (IOException e) {
-            assertTrue(logs.isEmpty());
+            // pass
         }
     }
 
     @Test
     public void testReaderEmptyfile() {
+        // testReaderEmptyLog.json file must contain: []
         JsonReader reader = new JsonReader("./data/testReaderEmptyLog.json");
         try {
-            List<Log> logs = reader.read();
-            System.out.println(logs.isEmpty());
-            assertTrue(logs.isEmpty());
+            List<WorkoutSession> sessions = reader.read();
+            System.out.println(sessions.isEmpty());
+            assertTrue(sessions.isEmpty());
         } catch (IOException e) {
             fail("Couldn't read from file");
         }
@@ -45,14 +42,26 @@ public class JsonReaderTest extends JsonTest {
     public void testReaderGeneral() {
         JsonReader reader = new JsonReader("./data/testReaderGeneral.json");
         try {
-            List<Log> logs = reader.read();
-            assertNotNull(logs);
-            assertEquals(3, logs.size());
-            checkExercise(logs.get(0), "Bench press", Muscles.CHEST, 100, 3, 12);
-            checkExercise(logs.get(1), "Shoulder press", Muscles.SHOULDERS, 50, 4, 11);
-            checkExercise(logs.get(2), "Squat", Muscles.LEGS, 90, 3, 10);
+            // This test needs testReaderGeneral.json to be in new format
+            List<WorkoutSession> sessions = reader.read();
+            assertEquals(2, sessions.size());
+            
+            // check session 1
+            WorkoutSession session1 = sessions.get(0);
+            assertEquals("2025/10/01", session1.getDate());
+            assertEquals(1, session1.getExercises().size());
+            // use the capitalization from the Exercise class
+            checkExercise(session1.getExercises().get(0), "Bench press", Muscles.CHEST, 100, 3, 12);
+
+            // check session 2
+            WorkoutSession session2 = sessions.get(1);
+            assertEquals("2025/10/03", session2.getDate());
+            assertEquals(2, session2.getExercises().size());
+            checkExercise(session2.getExercises().get(0), "Squat", Muscles.LEGS, 200, 3, 5);
+            checkExercise(session2.getExercises().get(1), "Leg press", Muscles.LEGS, 300, 4, 10);
+            
         } catch (IOException e) {
-            // fail("Couldn't read from file");
+            fail("Couldn't read from file: " + e.getMessage());
         }
     }
 }

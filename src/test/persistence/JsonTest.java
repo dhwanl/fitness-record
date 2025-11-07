ebc6b2034c7c4b2f5315a2f6eaa4c78e77d95920
Donghwan Lee <dhwanyi@gmail.com>
2025-11-07 08:37:39 -0800

    Update JsonTest file

--- src/test/persistence/JsonTest.java
+++ src/test/persistence/JsonTest.java
@@ -1,22 +1,21 @@
 package persistence;
 
-import static org.junit.Assert.assertEquals;
-
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import model.Exercise;
-import model.Log;
 import model.Muscles;
 
 // Referenced from JsonSerialization Demo
-// https://github.students.cs.ubc.ca/CPSC210/JsonSerializationDemo
 public class JsonTest {
-    protected void checkExercise(Log log, String exerciseName, Muscles muscleType, 
-                                            int weightLifted, int numSets, int numReps) {
-        Exercise temp = log.getExercise();
+    /*
+     * Helper to check if an Exercise object has the correct fields.
+     */
+    protected void checkExercise(Exercise exercise, String exerciseName, Muscles muscleType, 
+                                        int weightLifted, int numSets, int numReps) {
         
-        assertEquals(temp.getExerciseName(), exerciseName);
-        assertEquals(temp.getMuscleType(), muscleType);
-        assertEquals(temp.getWeightLifted(), weightLifted);
-        assertEquals(temp.getNumSets(), numSets);
-        assertEquals(temp.getNumReps(), numReps);
+        assertEquals(exerciseName, exercise.getExerciseName());
+        assertEquals(muscleType, exercise.getMuscleType());
+        assertEquals(weightLifted, exercise.getWeightLifted());
+        assertEquals(numSets, exercise.getNumSets());
+        assertEquals(numReps, exercise.getNumReps());
     }
 }
